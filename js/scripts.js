f395201fa6c79984983ccbc3057da12005220913
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-10 02:54:18 +0300

    add test for consonants.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 function pigLatin(word) {
   const vowels = ["a", "e", "i", "o", "u"];
   if (vowels.includes(word[0])) {
-   return word + "way";
+    return word + "way";
   }
-  
+
+  let editedWord = word
+  let firstCharacter = editedWord.charAt(0)
+
+  return word.slice(1) + firstCharacter + "ay"
 }
